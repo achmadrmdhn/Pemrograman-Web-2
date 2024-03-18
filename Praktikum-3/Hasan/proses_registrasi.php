00abfb35f4ed8eafcae9e335ce3262db16cf4c06
achmadrmdhn <achmadrifai0511@gmail.com>
2024-03-18 08:44:11 +0700

    test update

---
+++ Praktikum-3/Hasan/proses_registrasi.php
@@ -0,0 +1,10 @@
+<?php
+
+$domisili = [
+    "Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital"
+];
