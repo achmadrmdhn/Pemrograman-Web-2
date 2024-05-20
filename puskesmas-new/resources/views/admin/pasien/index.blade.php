a6a2bf5952bb84126f2b4472d71a9282308f07ee
achmadrmdhn <achmadrifai0511@gmail.com>
2024-05-20 15:31:33 +0700

    praktikum pekan 10

--- puskesmas-new/resources/views/admin/pasien/index.blade.php
+++ puskesmas-new/resources/views/admin/pasien/index.blade.php
@@ -8,18 +8,26 @@
                 <th>Id</th>
                 <th>Kode Pasien</th>
                 <th>Nama</th>
+                <th>Tempat Lahir</th>
                 <th>Tanggal Lahir</th>
                 <th>Gender</th>
                 <th>Email</th>
+                <th>Alamat</th>
                 <th>Nama Kelurahan</th>
             </tr>
+            @foreach ($pasiens as $pasien)
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>{{ $pasien->id }}</td>
+                <td>{{ $pasien->kode }}</td>
+                <td>{{ $pasien->nama }}</td>
+                <td>{{ $pasien->tmp_lahir }}</td>
+                <td>{{ $pasien->tgl_lahir }}</td>
+                <td>{{ $pasien->gender }}</td>
+                <td>{{ $pasien->email }}</td>
+                <td>{{ $pasien->alamat }}</td>
+                <td>{{ $pasien->kelurahan_nama }}</td>
             </tr>
+            @endforeach
         </table>
     </x-slot>
 </x-layout>
