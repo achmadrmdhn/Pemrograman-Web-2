4a307c731f37dcdcf65002a930e219f14105d88b
achmadrmdhn <achmadrifai0511@gmail.com>
2024-06-10 15:48:08 +0700

    auth pekan 13

--- puskesmas-new/routes/web.php
+++ puskesmas-new/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ContactController;
@@ -10,6 +11,34 @@ use App\Http\Controllers\KelurahanController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('/dashboard')->group(function () {
+        Route::get('/', [AdminController::class, 'index']);
+
+        // Auth Kelurahan
+        Route::prefix('/kelurahan')->group(function () {
+            Route::get('/', [KelurahanController::class, 'index']);
+            Route::get('/create', [KelurahanController::class, 'create']);
+            Route::post('/store', [KelurahanController::class, 'store']);
+            Route::get('/show/{id}', [KelurahanController::class, 'show']);
+            Route::get('/edit/{id}', [KelurahanController::class, 'edit']);
+            Route::put('/update/{id}', [KelurahanController::class, 'update']);
+            Route::delete('/destroy/{id}', [KelurahanController::class, 'destroy']);
+        });
+    });
+});
+
+
+require __DIR__.'/auth.php';
 
 Route::get('/salam', function () {
     return "Selamat Belajar Bareng Saya";
@@ -33,9 +62,9 @@ Route::get('/dashboard/pasien', [PasienController::class, 'index']);
 Route::get('/dashboard/kelurahan', [KelurahanController::class, 'index']);
 
 // Praktikum Laravel 4
-Route::get('/dashboard/kelurahan/create', [KelurahanController::class, 'create']);
-Route::post('/dashboard/kelurahan/store', [KelurahanController::class, 'store']);
-Route::get('/dashboard/kelurahan/show/{id}', [KelurahanController::class, 'show']);
+// Route::get('/dashboard/kelurahan/create', [KelurahanController::class, 'create']);
+// Route::post('/dashboard/kelurahan/store', [KelurahanController::class, 'store']);
+// Route::get('/dashboard/kelurahan/show/{id}', [KelurahanController::class, 'show']);
 
 // Tugas Praktikum Laravel 4
 Route::get('/dashboard/pasien/create', [PasienController::class, 'create']);
@@ -43,6 +72,10 @@ Route::post('/dashboard/pasien/store', [PasienController::class, 'store']);
 Route::get('/dashboard/pasien/show/{id}', [PasienController::class, 'show']);
 
 // Praktikum Laravel 5
-Route::get('/dashboard/kelurahan/edit/{id}', [KelurahanController::class, 'edit']);
-Route::put('/dashboard/kelurahan/update/{id}', [KelurahanController::class, 'update']);
-Route::delete('/dashboard/kelurahan/destroy/{id}', [KelurahanController::class, 'destroy']);
+// Route::get('/dashboard/kelurahan/edit/{id}', [KelurahanController::class, 'edit']);
+// Route::put('/dashboard/kelurahan/update/{id}', [KelurahanController::class, 'update']);
+// Route::delete('/dashboard/kelurahan/destroy/{id}', [KelurahanController::class, 'destroy']);
+
+Route::get('/dashboard/pasien/edit/{id}', [PasienController::class, 'edit']);
+Route::put('/dashboard/pasien/update/{id}', [PasienController::class, 'update']);
+Route::delete('/dashboard/pasien/destroy/{id}', [PasienController::class, 'destroy']);
