e42991b2c3d2460ee96ce7a3299f4b5636e1deef
achmadrmdhn <achmadrifai0511@gmail.com>
2024-06-18 16:05:21 +0700

    updated

--- puskesmas-new/resources/views/admin/pasien/index.blade.php
+++ puskesmas-new/resources/views/admin/pasien/index.blade.php
@@ -38,7 +38,7 @@
                 <td>{{ $pasien->kelurahan_nama }}</td>
                 <td>
                     <a href="{{ url('dashboard/pasien/show', $pasien->id) }}" class="btn btn-primary btn-sm"><i class="far fa-eye"></i> Lihat<a>
-                    <a href="{{ url('dashboard/pasien/show', $pasien->id) }}" class="btn btn-warning btn-sm"><i class="far fa-edit"></i> Edit</a>
+                    <a href="{{ url('dashboard/pasien/edit', $pasien->id) }}" class="btn btn-warning btn-sm"><i class="far fa-edit"></i> Edit</a>
                     <form class="forms-sample d-inline" action="{{ url('dashboard/pasien/destroy', $pasien->id) }}" method="POST">
                         @csrf
                         @method('delete')
